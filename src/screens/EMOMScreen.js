0ca12ae4f4ea688bedb327dc231b7a691528e0e6
Eemrson <emerson.olivap@gmail.com>
2020-08-07 21:15:09 -0300

    criacao das telas emo e homescreen

---
+++ src/screens/EMOMScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+
+const EMOMScreen= props =>{
+
+return(
+
+    <View style= {styles.container}>
+        <Text>EMOM Sreen</Text>
+    </View>
+)
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#D6304A'
+    }})
+export default EMOMScreen
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {View,Text, TouchableOpacity, StyleSheet} from 'react-native'
+import { createStackNavigator,NavigationStackOptions } from 'react-navigation-stack';
+import { createAppContainer} from 'react-navigation';
+
+import Button from '../components/Button'
+const HomeScreen= props =>{
+
+return(
+
+    <View style={styles.container} >
+
+         <Text style= {styles.logo}>Calistimer</Text>
+          <Button style={styles.btn} onPress={()=> props.navigation.navigate('EMOM')}>Emom</Button>
+          <Button style={styles.btn} onPress={()=> props.navigation.navigate('EMOM')}>ARMAP</Button>
+          <Button style={styles.btn} onPress={()=> props.navigation.navigate('EMOM')}>Isometria</Button>
+   
+   
+    </View>
+)
+
+}
+HomeScreen.NavigationStackOptions ={
+header: null
+}
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#D6304A'
+    },
+    logo: {fontFamily: 'Ubuntu-Bold', fontSize: 48, textAlign: 'center',color:111},
+ btn: {padding:20}
+}
+
+)
+export default HomeScreen
